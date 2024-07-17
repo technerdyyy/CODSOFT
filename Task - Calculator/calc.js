b1843a086d48491289ab4b8759c726dacd376f66
technerdyyy <akansha7803@gmail.com>
2024-07-17 17:17:20 +0530

    Initial commit

---
+++ Task - Calculator/calc.js
@@ -0,0 +1,51 @@
+const input = document.querySelector("#input-box");
+const buttons = document.querySelectorAll("button");
+const operators = ["%", "/", "*", "-", "+", "="];
+let output = "";
+let lastBtnEquals = false;
+
+
+const calculate = (btnVal) => {
+    if (btnVal === "=" && output !== "") {
+        output = eval(output);
+        lastBtnEquals = true;
+    } else if (btnVal === "AC") {
+        output = "";
+        lastWasEqual = false;
+
+    } else if (btnVal === "DEL") {
+        output = output.slice(0, -1);
+        lastBtnEquals = false;
+    
+    } else if (btnVal === "%") {
+        output = (parseFloat(output) / 100).toString();
+         
+    } else {
+        if (lastBtnEquals) {     // if the last button entered is "=" then :
+            if (operators.includes(btnVal)) {     // if the last button entered is any operator then :
+                lastBtnEquals = false;     // it implies that last button was not "=" and we will continue using the obtained result for next operation
+            } else {
+                output = "";
+                lastBtnEquals = false;     // else starting a fresh operation
+            }
+        }
+        if (output === "" && operators.includes(btnVal)) {     // if there is nothing on screen and the first button pressed is an operator
+            return; // no change displayed on screen
+        }
+        output += btnVal;
+        
+    }
+    input.value = output;
+};
+
+
+
+//adding event listener to buttons
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        let btnVal = e.target.dataset.value;
+        if (btnVal) {
+            calculate(btnVal);
+        }
+    });
+});
